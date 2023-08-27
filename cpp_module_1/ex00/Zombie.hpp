1779eaa330a32a10ab29840a0f9006fc99ffb294
Albina Peposhi <apeposhi@2-I-1.42heilbronn.de>
2023-08-27 14:36:09 +0200

    Added cpp_module_1 code

---
+++ cpp_module_1/ex00/Zombie.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/27 14:12:19 by apeposhi          #+#    #+#             */
+/*   Updated: 2023/08/27 14:23:59 by apeposhi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_H
+#define ZOMBIE_H
+
+#include <iostream>
+
+class Zombie {
+private:
+	std::string name;
+public:
+	Zombie();
+	~Zombie();	
+};
+
+#endif
