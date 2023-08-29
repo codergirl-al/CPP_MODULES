251d0a28d35400bf46059b38bb1f4eb23762d64e
Albina Peposhi <apeposhi@3-G-4.42heilbronn.de>
2023-08-29 15:52:44 +0200

    Added changes

--- cpp_module_1/ex00/Zombie.hpp
+++ cpp_module_1/ex00/Zombie.hpp
@@ -6,21 +6,27 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/27 14:12:19 by apeposhi          #+#    #+#             */
-/*   Updated: 2023/08/27 14:23:59 by apeposhi         ###   ########.fr       */
+/*   Updated: 2023/08/29 12:43:42 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ZOMBIE_H
-#define ZOMBIE_H
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
 
 #include <iostream>
 
 class Zombie {
 private:
-	std::string name;
+    std::string name;
+
 public:
-	Zombie();
-	~Zombie();	
+    Zombie(std::string name);
+    ~Zombie();
+    
+    void announce(void);
 };
+
+Zombie* newZombie(std::string name);
+void randomChump(std::string name);
 
 #endif
