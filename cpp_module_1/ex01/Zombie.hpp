c9addccce829b312e1d457d8e1ac9d446be19580
Albina Peposhi <codegirl.al@gmail.com>
2023-09-14 10:00:22 +0200

    CPP01

---
+++ cpp_module_1/ex01/Zombie.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/27 14:12:19 by apeposhi          #+#    #+#             */
+/*   Updated: 2023/08/30 17:37:50 by apeposhi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <iostream>
+
+class Zombie {
+private:
+    std::string name;
+
+public:
+    Zombie(std::string name);
+    ~Zombie();
+    
+    void announce(void);
+};
+
+Zombie* newZombie(std::string name);
+Zombie* zombieHorde(int N, std::string name);
+void randomChump(std::string name);
+
+#endif
