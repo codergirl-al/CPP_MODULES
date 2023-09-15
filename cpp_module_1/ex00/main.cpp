1a239d0a6300bb6ffa2d417d0ea29e0c2a2518ea
Albina Peposhi <codegirl.al@gmail.com>
2023-09-15 12:34:15 +0200

    ex02

--- cpp_module_1/ex00/main.cpp
+++ cpp_module_1/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/27 14:12:17 by apeposhi          #+#    #+#             */
-/*   Updated: 2023/08/30 17:18:31 by apeposhi         ###   ########.fr       */
+/*   Updated: 2023/08/30 17:38:39 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,16 @@ int main() {
 
     // Test 2: Creating and announcing a Zombie on the stack
     randomChump("StackZombie");
+
+	// Test 3: Creating a horde of Zombies
+    int N = 5;
+    Zombie* horde = zombieHorde(N, "HordeZombie");
+    
+    for (int i = 0; i < N; ++i) {
+        horde[i].announce();
+    }
+    
+    delete[] horde; // Free the allocated memory for the horde
 
     return 0;
 }
---
+++ cpp_module_1/ex02/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/14 15:27:54 by apeposhi          #+#    #+#             */
+/*   Updated: 2023/09/15 11:36:39 by apeposhi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main() {
+	// Declaration and Initialization
+	std::string str = "HI THIS IS BRAIN";
+	std::string* stringPTR = &str;
+	std::string& stringREF = str;
+
+	// Print memory addresses
+	std::cout << "The memory address of the string variable: " << &str << std::endl;
+	std::cout << "The memory address held by stringPTR: " << &str << std::endl;
+	std::cout << "The memory address of the string variable: " << &str << std::endl;
+
+	// Values
+	std::cout << "String value: " << str << std::endl;
+	std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+	
+	return (0);
+}
