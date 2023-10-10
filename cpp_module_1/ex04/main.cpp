f293b3fb4d63b72454c7acf7cc00ad2488b877cb
Albina Peposhi <apeposhi@1-B-23.42heilbronn.de>
2023-10-10 23:24:26 +0200

    Added changes

--- cpp_module_1/ex04/main.cpp
+++ cpp_module_1/ex04/main.cpp
@@ -6,13 +6,55 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 22:42:09 by apeposhi          #+#    #+#             */
-/*   Updated: 2023/10/09 12:48:03 by apeposhi         ###   ########.fr       */
+/*   Updated: 2023/10/10 23:08:54 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <fstream>
+
+bool replaceAndSave(const std::string& filename, const std::string& s1, const std::string& s2) {
+    std::ifstream fileIn(filename);
+    if (!fileIn.is_open()) {
+        std::cerr << "Error: Cannot open file: " << filename << std::endl;
+        return (false);
+    }
+    std::string content((std::istreambuf_iterator<char>(fileIn)), std::istreambuf_iterator<char>());
+    fileIn.close();
+
+    size_t position = 0;
+    while ((position = content.find(s1, position)) != std::string::npos) {
+        content.erase(position, s1.length());
+        content.insert(position, s2);
+        position += s2.length();
+    }
+
+    std::ofstream fileOut(filename + ".replace");
+    if (!fileOut.is_open()) {
+        std::cerr << "Error: Cannot create output file: " << filename << ".replace" << std::endl;
+        return (false);
+    }
+
+    fileOut << content;
+    fileOut.close();
+    return (true);
+}
 
 int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " <filename> <string1> <string2>" << std::endl;
+        return (0);
+    }
+    std::string filename(argv[1]);
+    std::string s1(argv[2]);
+    std::string s2(argv[3]);
+    
+    if (s1.empty()) {
+        std::cerr << "Error: String to replace (s1) must not be empty." << std::endl;
+        return (1);
+    }
 
-  return (0);
+    if (!replaceAndSave(filename, s1, s2))
+        return (1);
+    return (0);
 }
---
+++ cpp_module_1/ex04/tester.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib> // for system()
+
+bool compareFiles(const std::string& p1, const std::string& p2) {
+    std::ifstream f1(p1, std::ifstream::binary|std::ifstream::ate);
+    std::ifstream f2(p2, std::ifstream::binary|std::ifstream::ate);
+
+    if (f1.fail() || f2.fail()) {
+        return false; // file problem
+    }
+
+    if (f1.tellg() != f2.tellg()) {
+        return false; // size mismatch
+    }
+
+    // seek back to beginning and use std::equal to compare contents
+    f1.seekg(0, std::ifstream::beg);
+    f2.seekg(0, std::ifstream::beg);
+    return std::equal(std::istreambuf_iterator<char>(f1.rdbuf()),
+                      std::istreambuf_iterator<char>(),
+                      std::istreambuf_iterator<char>(f2.rdbuf()));
+}
+
+bool testReplacement(const std::string& input, 
+                     const std::string& s1, 
+                     const std::string& s2, 
+                     const std::string& expectedOutput) 
+{
+    std::system(("./replaceAndSave " + input + " " + s1 + " " + s2).c_str());
+
+    bool isSame = compareFiles(input + ".replace", expectedOutput);
+    if (!isSame) {
+        std::cerr << "Test failed for: " << input << " " << s1 << " " << s2 << "\n";
+        std::cerr << "See: " << input + ".replace" << "\n";
+        std::cerr << "Expected: " << expectedOutput << "\n";
+    }
+    return isSame;
+}
+
+int main() {
+    bool allTestsPassed = true;
+
+    // Test 1: Basic Replacement
+    if (!testReplacement("test1_input.txt", "World", "Planet", "test1_expected_output.txt")) {
+        allTestsPassed = false;
+    }
+
+    // Test 2: No Replacement (s1 not in file)
+    if (!testReplacement("test2_input.txt", "Nonexistent", "Replacement", "test2_expected_output.txt")) {
+        allTestsPassed = false;
+    }
+
+    // Test 3: s1 is a subset of s2
+    if (!testReplacement("test3_input.txt", "Cat", "Catastrophe", "test3_expected_output.txt")) {
+        allTestsPassed = false;
+    }
+
+    // Test 4: s1 is longer than s2
+    if (!testReplacement("test4_input.txt", "Dinosaur", "Dog", "test4_expected_output.txt")) {
+        allTestsPassed = false;
+    }
+
+    // Test 5: s1 is a special character
+    if (!testReplacement("test5_input.txt", "$", "Dollar", "test5_expected_output.txt")) {
+        allTestsPassed = false;
+    }
+
+    // Test 6: s1 and s2 are numbers
+    if (!testReplacement("test6_input.txt", "123", "OneTwoThree", "test6_expected_output.txt")) {
+        allTestsPassed = false;
+    }
+
+    // Test 7: s1 and s2 are empty strings
+    if (!testReplacement("test7_input.txt", "", "", "test7_expected_output.txt")) {
+        allTestsPassed = false;
+    }
+
+    // Test 8: s1 is space character
+    if (!testReplacement("test8_input.txt", " ", "SPACE", "test8_expected_output.txt")) {
+        allTestsPassed = false;
+    }
+
+    // Add more tests as needed...
+
+    if (allTestsPassed) {
+        std::cout << "All tests passed!\n";
+    } else {
+        std::cerr << "Some tests failed!\n";
+    }
+
+    return 0;
+}
