a4b24f358451eaf5d4cc519f27f9002ad1bbe8ea
Albina Peposhi <apeposhi@1-B-23.42heilbronn.de>
2023-10-10 23:25:07 +0200

    Cleanup

--- cpp_module_1/ex03/HumanB.cpp
+++ cpp_module_1/ex03/HumanB.cpp
@@ -6,23 +6,21 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/16 20:19:58 by apeposhi          #+#    #+#             */
-/*   Updated: 2023/09/17 22:40:00 by apeposhi         ###   ########.fr       */
+/*   Updated: 2023/10/10 22:55:02 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanB.hpp"
 
-HumanB::HumanB(const std::string& name) : name(name), weapon(0) {} // Initialization in the constructor's initializer list
+HumanB::HumanB(const std::string& name) : name(name), weapon(0) {}
 
 void HumanB::setWeapon(Weapon& weapon) {
     this->weapon = &weapon;
 }
 
 void HumanB::attack() const {
-    if (weapon) {
+    if (weapon)
         std::cout << name << " attacks with their " << weapon->getType() << std::endl;
-    }
-    else {
+    else
         std::cout << name << " has no weapon!" << std::endl;
-    }
 }
